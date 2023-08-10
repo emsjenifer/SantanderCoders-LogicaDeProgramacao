a59ac03ec80c320fc06efd7009ab823c17b4ad04
emsjenifer <emsjenifer@gmail.com>
2023-08-09 22:28:23 -0300

    Lista 02 - exercícios 1 à 12

---
+++ Listas/Lista 02/lista-02.js
@@ -0,0 +1,262 @@
+// ### Dicionários (Objetos):
+
+// 1- Crie um objeto que represente um estudante com propriedades como nome, idade, curso e notas. Acesse e exiba essas propriedades.
+const estudante = {
+  nome: 'Emily',
+  idade: 28,
+  curso: 'Sistemas de Informação',
+  notas: [80, 78, 95, 67]
+}
+
+const informaDadosEstudante = () => {
+  console.log(`A estudante ${estudante.nome} tem ${estudante.idade}, cursa ${estudante.curso} e tiras as seguintes notas no 1º semestre de 2023:`)
+  console.log(`Desenvolvimento Web: ${estudante.notas[0]}`)
+  console.log(`Engenharia de Software: ${estudante.notas[1]}`)
+  console.log(`Análise de Sistemas: ${estudante.notas[2]}`)
+  console.log(`Cálculo: ${estudante.notas[3]}`)
+}
+
+informaDadosEstudante()
+
+// 2- Crie um dicionário que mapeie nomes de países a suas respectivas capitais. Crie uma função que receba o nome de um país e retorne a capital correspondente.
+const paises = {
+  Angola: 'Luanda',
+  Brasil: 'Brasília',
+  Canadá: 'Ottawa',
+  Dinamarca: 'Roseau',
+  Etiópia: 'Adis Abeba'
+}
+
+let nomePais = prompt('Digite o nome do país: ')
+const encontraCapital = (nomePais, paises) => paises.hasOwnProperty(nomePais) ? `A capital de ${nomePais} é ${paises[nomePais]}` : `Capital não encontrada`
+
+console.log(encontraCapital(nomePais, paises))
+
+// 3- Crie um dicionário com nomes de frutas e seus preços. Escreva uma função que calcule o preço total de uma cesta de frutas.
+const inventarioHortifrut = {
+  banana: 12.99,
+  maçã: 5.98,
+  morango: 6.99,
+  pêra: 15.87,
+  uva: 8.59,
+}
+
+const checkoutHortifrut = () => {
+  let decisao = 's'
+  let total = 0
+  while (decisao  === 's') {
+    item = prompt('Digite o item que deseja adicionar no carrinho:')
+    peso = prompt('Digite a quantidade em quilos que deseja do produto:')
+    total += (inventarioHortifrut[item]) * peso
+
+    decisao = prompt('Deseja continuar comprando? (s/n)')
+  }
+
+  return `A sua compra ficou em um total de R$ ${total.toFixed(2)}`
+}
+
+console.log(checkoutHortifrut())
+
+// 4- Crie um objeto que represente um livro com propriedades como título, autor, ano de publicação, etc. Crie uma função que imprima as informações do livro em um formato legível.
+const codigoLimpo = {
+  Título: 'Código Limpo: Habilidades Práticas do Agile Software',
+  Autor:'Robert C. Martin',
+  Genero: 'Programação',
+  Páginas: 464,
+  Editora: 'Alta Books',
+}
+
+const mostraLivro = () => {
+  for(let propriedades in codigoLimpo) {
+    console.log(`${propriedades}: ${codigoLimpo[propriedades]}`)
+  }
+}
+
+mostraLivro()
+
+//### Map:
+
+// 5- Dado um array de números, crie um novo array onde cada número seja elevado ao quadrado.
+const numeros = [2, 8, 4, 3, 7]
+const numerosAoQuadrado = numeros => numeros.map((numero) => numero ** 2)
+
+console.log(numerosAoQuadrado(numeros))
+
+// 6- Dado um array de palavras, crie um novo array com a primeira letra de cada palavra em maiúscula.
+const palavras = ['pop', 'push', 'reduce', 'reverse', 'slice', 'sort']
+const retornaPrimeiraLetra = palavras => palavras.map((palavra) => palavra[0].toUpperCase())
+
+console.log(retornaPrimeiraLetra(palavras))
+
+// 7- Crie um array de objetos representando produtos com propriedades como nome, preço e quantidade. Use o método map() para calcular o valor total de cada produto (preço * quantidade).
+const produtosNerd = [
+  {
+    nome: "Camiseta Star Wars",
+    preco: 39.99,
+    quantidade: 10
+  },
+  {
+    nome: "Funko Pop Homem de Ferro",
+    preco: 15.99,
+    quantidade: 5
+  },
+  {
+    nome: "Caneca Harry Potter",
+    preco: 12.49,
+    quantidade: 15
+  }
+]
+
+const calculaValorTotal = produtosNerd => produtosNerd.map((produto) => `O valor total do produto ${produto.nome} é R$ ${(produto.preco * produto.quantidade).toFixed(2)}`)
+
+console.log(calculaValorTotal(produtosNerd))
+
+// 8- Converta um array de temperaturas em graus Celsius para Fahrenheit usando a fórmula (C * 9/5) + 32.
+const temperaturas = [15, 22, 17, 32, 27]
+const converteTemperatura = temperaturas => temperaturas.map((temperatura) => `${temperatura} graus Celsius equivale a ${(temperatura * 9/5) + 32} graus na escala Fahrenheit`)
+
+console.log(converteTemperatura(temperaturas))
+
+//### Reduce:
+
+// 9- Dado um array de números, use o método reduce() para calcular a soma de todos os elementos.
+const conjuntoNumeros = [12, 15, 5, 22, 1]
+const somaNumeros = conjuntoNumeros => conjuntoNumeros.reduce((acumulador, valor) => acumulador + valor, 0)
+
+console.log(somaNumeros(conjuntoNumeros))
+
+// 10- Crie um array de strings e use o método reduce() para concatenar todas as strings em uma única string.
+const conjuntoPalavras = ['ol', 'df', 'aq', 'ws']
+const concatenaPalavras = conjuntoPalavras => conjuntoPalavras.reduce((acumulador, palavra) => acumulador + palavra)
+
+console.log(concatenaPalavras(conjuntoPalavras))
+
+// 11- Dado um array de objetos representando despesas com propriedades como valor e categoria, use o método reduce() para calcular o total de despesas de uma categoria específica.
+const despesasEmpresa = [
+  {
+    categoria: "Salários",
+    valor: [4056, 10256, 8450, 6450]
+  },
+  {
+    categoria: "Desenvolvimento de Software",
+    valor: [22000, 57800, 65400, 15600]
+  },
+  {
+    categoria: "Marketing",
+    valor: [250, 540, 1200, 780]
+  },
+  {
+    categoria: "Servidores",
+    valor: [1230, 4581, 4650, 15602]
+  }
+]
+
+const somaDespesas = despesasEmpresa => despesasEmpresa.map((despesa) => 
+  `O total de despesas da categoria ${despesa.categoria} é de R$ ${despesa.valor.reduce((despesaAccumulator, valor) => despesaAccumulator + valor, 0)}`)
+
+console.log(somaDespesas(despesasEmpresa))
+
+// 12- Dado um array de números, use o método reduce() para encontrar o maior número no array.
+const algunsNumeros = [98, 56, 75, 35, 65, 45, 78]
+const encontraMaiorNumero = algunsNumeros => algunsNumeros.reduce((maior, numero) => 
+  numero > maior ? numero : maior, algunsNumeros[0])
+
+console.log(encontraMaiorNumero(algunsNumeros))
+
+// ### Laços de Repetiçoes
+// 13- Imprima os números de 1 a 10 utilizando um loop for.
+
+// 14- Imprima os números pares de 1 a 20 utilizando um loop for.
+
+// 15- Calcule a soma dos números de 1 a 50 utilizando um loop for.
+
+// 16- Imprima a tabuada do 7 utilizando um loop for.
+
+// 17- Escreva um programa que solicite ao usuário um número e imprima a sequência de números de 1 até o número inserido, utilizando um loop for.
+
+// 18- Escreva um programa que solicite ao usuário um número e verifique se ele é primo.
+
+// 19- Crie um programa que gere a sequência de Fibonacci até o décimo termo. (Dica: o próximo termo é a soma dos dois termos anteriores)
+
+// 20- Crie um programa que solicite ao usuário um número e calcule o fatorial desse número.
+
+// 21- Escreva um programa que imprima a seguinte série: 1, 3, 6, 10, 15, 21, ..., onde cada número é a soma dos números naturais consecutivos.
+
+// 22- Crie um programa que encontre e imprima todos os números perfeitos de 1 a 1000. Um número perfeito é aquele cuja soma de seus divisores (excluindo ele mesmo) é igual ao próprio número.
+
+// 23- Crie um jogo em que o computador escolhe um número aleatório entre 1 e 100, e o jogador deve adivinhar qual é esse número. O jogo deve fornecer dicas informando se o número a ser adivinhado é maior ou menor do que o palpite do jogador, e o jogo deve continuar até que o jogador acerte o número.
+
+// 24- Crie um programa que gere a sequência de números de Fibonacci usando um loop while até que o próximo termo seja maior que 1000.
+
+// 25- Escreva um programa que calcule a média de uma lista de números. O usuário deve fornecer quantos números deseja inserir, e em seguida, inserir os números.
+
+// 26- Escreva um programa que peça ao usuário para adivinhar um número secreto entre 1 e 100. O programa deve fornecer dicas como "maior" ou "menor" até que o jogador adivinhe corretamente.
+
+// 27- Crie um programa que gere a sequência de números primos até um número fornecido pelo usuário.
+
+// 28- Escreva um código JavaScript que utilize um laço de repetição while para imprimir os números pares de 0 a 10.
+
+// 29- Escreva um código JavaScript que solicite ao usuário um número e utilize um laço de repetição do-while para verificar se o número inserido é uma potência de 2.
+
+// 30- Escreva um código JavaScript que utilize um laço de repetição while para calcular a soma dos números de 1 a 100 e exiba o resultado.
+
+// ### Condicionais
+
+// 31- Verificação de vogal ou consoante
+
+// Peça ao usuário para digitar uma letra.
+// Use uma estrutura condicional para verificar se a letra é uma vogal ou uma consoante.
+// Exiba uma mensagem indicando se a letra é uma vogal ou consoante.
+
+// 32- Calculadora simples
+
+// Peça ao usuário para digitar dois números e uma operação matemática (+, -, *, /).
+// Use uma estrutura condicional para realizar a operação selecionada nos números dados.
+// Exiba o resultado da operação.
+
+// 33- Verificação de número primo
+
+// Peça ao usuário para digitar um número.
+// Use uma estrutura condicional para verificar se o número é primo (divisível apenas por 1 e por ele mesmo).
+// Exiba uma mensagem indicando se o número é primo ou não.
+
+// 34- Conversão de temperatura
+
+// Peça ao usuário para digitar uma temperatura em graus Celsius.
+// Use uma estrutura condicional para converter a temperatura para Fahrenheit ou Kelvin, de acordo com a escolha do usuário.
+// Exiba o resultado da conversão.
+
+// 35- Dia da semana por extenso
+
+// Peça ao usuário para digitar um número de 1 a 7 representando um dia da semana.
+// Use uma estrutura condicional switch para exibir o nome completo do dia da semana correspondente ao número digitado.
+
+// 36- Verificação de ano bissexto
+
+// Peça ao usuário para digitar um ano.
+// Use uma estrutura condicional para verificar se o ano é bissexto (divisível por 4 e não por 100, exceto se for divisível por 400).
+// Exiba uma mensagem indicando se o ano é bissexto ou não.
+
+// 37- Contador de vogais e consoantes
+
+// Peça ao usuário para digitar uma palavra ou frase.
+// Use uma estrutura condicional para contar o número de vogais e consoantes na entrada.
+// Exiba os resultados separadamente.
+
+// 38- Verificação de número positivo, negativo ou zero
+
+// Peça ao usuário para digitar um número.
+// Use uma estrutura condicional para verificar se o número é positivo, negativo ou zero.
+// Exiba uma mensagem indicando a natureza do número.
+
+// 39- Sistema de bilhete de cinema
+
+// Peça ao usuário para digitar sua idade e se tem cartão de estudante (responda com "sim" ou "não").
+// Use uma estrutura condicional para determinar o preço do bilhete de cinema de acordo com as regras: menores de 18 anos pagam $5, estudantes pagam $8 e os demais pagam $10.
+// Exiba o preço do bilhete.
+
+// 40- Verificação de palíndromo
+
+// Peça ao usuário para digitar uma palavra ou frase.
+// Use uma estrutura condicional para verificar se a entrada é um palíndromo (lê-se igual de trás para frente e de frente para trás).
+// Exiba uma mensagem indicando se a entrada é um palíndromo ou não.
